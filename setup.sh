98406a629fc67bc05f0b2cfd6ed9f1d37f41a6ae
Yongting You <2010youy01@gmail.com>
2020-10-08 12:24:49 -0700

    fix bug

--- setup.sh
+++ setup.sh
@@ -2,11 +2,6 @@ echo "Setup dotfiles"
 path=$(dirname "$0")
 path=$(realpath "$path")
 
-# Install oh-my-zsh
-./oh-my-zsh-setup.sh
-# oh-my-zsh theme
-cp $path/yyt.zsh-theme $HOME/.oh-my-zsh/themes/yyt.zsh-theme
-
 # backup old setups
 mv $HOME/.vim $HOME/.vim-backup
 mv $HOME/.tmux.conf $HOME/.tmux.conf-backup
@@ -14,9 +9,13 @@ mv $HOME/.oh-my-zsh $HOME/.oh-my-zsh-backup
 mv $HOME/.vimrc $HOME/.vimrc-backup
 mv $HOME/.zshrc $HOME/.zshrc-backup
 
+# Install oh-my-zsh
+./oh-my-zsh-setup.sh
+# oh-my-zsh theme
+cp $path/yyt.zsh-theme $HOME/.oh-my-zsh/themes/yyt.zsh-theme
+
 # create symbolic links
 ln -s $path/.vim $HOME/.vim
 ln -s $path/.tmux.conf $HOME/.tmux.conf
-ln -s $path/.oh-my-zsh $HOME/.oh-my-zsh
 ln -s $path/.vimrc $HOME/.vimrc
 ln -s $path/.zshrc $HOME/.zshrc
