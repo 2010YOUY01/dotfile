082b8c88556619c4cb497391a53379a84fe50dee
Yongting You <2010youy01@gmail.com>
2020-10-26 12:31:04 +0800

    leetcode config

--- .zshrc
+++ .zshrc
@@ -99,6 +99,7 @@ source $ZSH/oh-my-zsh.sh
 # Example aliases
 # alias zshconfig="mate ~/.zshrc"
 # alias ohmyzsh="mate ~/.oh-my-zsh"
+alias lc="leetcode"
 
 # fzf default setup
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
--- setup.sh
+++ setup.sh
@@ -17,6 +17,5 @@ mv $HOME/.zshrc $HOME/.zshrc-backup
 # create symbolic links
 ln -s $path/.vim $HOME/.vim
 ln -s $path/.tmux.conf $HOME/.tmux.conf
-ln -s $path/.oh-my-zsh $HOME/.oh-my-zsh
 ln -s $path/.vimrc $HOME/.vimrc
 ln -s $path/.zshrc $HOME/.zshrc
